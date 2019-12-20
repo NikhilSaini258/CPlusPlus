49d5b1f6ee1f00efd90e52dcc825c772df1143dd
NikhilSaini258 <nikhilsaini258@gmail.com>
2019-12-20 14:33:38 +0530

    Update Stack_Explicit_Constructor.cpp

--- Data Structure/Stack_Explicit_Constructor.cpp
+++ Data Structure/Stack_Explicit_Constructor.cpp
@@ -22,9 +22,8 @@ void fun2(vector<int> v) {
 
 int main()
 {
-    //us.insert(10, 20, 11, 34, 99, 86);
     vector<int> v;
-    Stack<int, vector<int>> s;
+    Stack<int, vector<int>> s;         // calls default constructor which result in calling of parameterized constructor
     //Stack<int, vector<int>> s2(v);   // calls parameterized constructor
 
     //fun2(v);                         // prints "vector", which is fine
@@ -32,6 +31,23 @@ int main()
     //fun(s);                          // prints "stack", which is fine
     fun(v);                            // prints "stack", when constructor is not explicit, which is wrong
                                        // This is the reason of putting, explicit constructor in std::stack od STL
+    
+    /* The constructors are explicit so you can't accidentally pass an underlying container (such as vector or deque) 
+    to a function expecting a stack, resulting in unexpected copying (not to mention violating the principle of least surprise). */
 
     return 0;
 }
+
+/* If a class has a constructor which can be called with a single argument, then this constructor becomes conversion constructor 
+   because such a constructor allows conversion of the single argument to the class being constructed.
+
+Example of implicit and explicit calls
+class ABC{
+public:
+    ABC(int) {}
+};
+
+ABC o2 = 100;          // implicit call
+ABC o1 = ABC(100);     // explicit call by type-casting
+
+*/
